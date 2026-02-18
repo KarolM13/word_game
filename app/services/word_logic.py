67499f857d60188422de1d40415dadc4c611dc65
KarolM13 <machkarol13@gmail.com>
2026-02-18 13:53:03 +0100

    Project structure , game logic

---
+++ app/services/word_logic.py
@@ -0,0 +1,41 @@
+class WordGame():
+    def __init__(self,word):
+        self.word = word.lower()
+        self.attemps = 0
+        self.max_attempts = 6
+        self.over = False
+        self.win = False
+    def MakeGuess(self,guess):
+        if self.over == True:
+            return {"error:" "Game is already over"}
+        self.attemps += 1
+        result = self.CheckLetters(guess.lower())
+        if guess.lower() == self.word:
+            self.win = True
+            self.over = True
+        elif self.attemps >= self.max_attempts:
+            self.over = True
+        
+        return{
+            "result": result,
+            "attemps":self.attemps,
+            "remaining":self.max_attempts - self.attemps,
+            "is_won": self.win,
+            "is_over": self.over
+        }
+    def CheckLetters(self,guess):
+        result = ["grey"] * len(guess)
+        used_letters = [0]* len(self.word)
+        word = self.word
+        for i in range(len(guess)):
+            if guess[i] == word[i]:
+                result[i] = "green"
+                used_letters[i] = 1
+        for i in range(len(guess)):
+            if result[i] != "green" and guess[i] in word:
+                for j in range(len(guess)):
+                    if word[j] == guess[i] and used_letters[j] == 0:
+                        result[i] = "yellow" 
+                        used_letters[j] =1
+                        break    
+        return result
