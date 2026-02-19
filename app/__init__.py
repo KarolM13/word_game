48fbf3cab28fe6516bc1c7fa1f743109f25254b4
KarolM13 <machkarol13@gmail.com>
2026-02-19 21:57:12 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 import os
 
 db = SQLAlchemy()
@@ -9,32 +8,28 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-     #   'DATABASE_URL', 'postgresql://admin:simple@db:5432/wordgame')
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    database_url = os.environ.get("DATABASE_URL")
-    if database_url:
-        database_url = database_url.replace("postgres://", "postgresql://")
+    
+    database_url = os.environ.get("DATABASE_URL", "postgresql://admin:simple@db:5432/wordgame")
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
     migrate.init_app(app, db)
     
-    # Blueprint API
     from app.routes.game_routes import game_bp
     app.register_blueprint(game_bp)
 
-    # Route dla strony głównej
     @app.route("/")
     def index():
         return render_template("index.html")
     
-    from app.models.word import Word
-    
     with app.app_context():
-        #db.create_all()
-        #from app.seeds import seed_words
-        #seed_words()
-        print("Dodano przykładowe słowa do bazy!")
+        from app.models.word import Word
+        db.create_all()
+        from app.seeds import seed_words
+        seed_words()
     
     return app
